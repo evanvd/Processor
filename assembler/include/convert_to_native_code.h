c58a5e95764ef9ff5754498fc06647710302c9fc
evanvd <ivanvdeev7347@gmail.com>
2025-10-17 19:49:58 +0300

    first version label

--- assembler/include/convert_to_native_code.h
+++ assembler/include/convert_to_native_code.h
@@ -19,6 +19,9 @@ enum op_code
     OP_JB = 52
 };
 
+
+static size_t labels[10] = {}; // TODO find labels
+void FindLabels (char* assembler_text);
 typedef bool (*args_fn) (char*, asm_t*);
 
 struct operation
@@ -26,11 +29,47 @@ struct operation
     const char* op_name = NULL;
     op_code operation_code;
 };
+struct args_op
+{
+    const char* op_name = NULL;
+    op_code operation_code;
+    args_fn fn = NULL;
+};
 
-void ConvertToNative(asm_t* assembler);
-assembler_err NativeTranslator(asm_t* assembler, char* assembler_text);
+
 bool ComparePush(char* assembler_text, asm_t* assembler);
 bool ComparePushR(char* assembler_text, asm_t* assembler);
 bool ComparePopR(char* assembler_text, asm_t* assembler);
 bool CompareJump(char* assembler_text, asm_t* assembler);
 bool CompareJB(char* assembler_text, asm_t* assembler);
+
+//static args_fn op_arg[] = {ComparePush, ComparePopR, ComparePushR, CompareJump, CompareJB};
+
+const int op_args_count = 5;
+const int simple_op_count = 7;
+
+static operation operation [simple_op_count] = 
+{
+    {.op_name = "POP", .operation_code = OP_POP},
+    {.op_name = "DUMP", .operation_code = OP_DUMP},
+    {.op_name = "MUL", .operation_code = OP_MUL},
+    {.op_name = "SUB", .operation_code = OP_SUB},
+    {.op_name = "OUT", .operation_code = OP_OUT},
+    {.op_name = "DIV", .operation_code = OP_DIV},
+    {.op_name = "HLT", .operation_code = OP_HLT}
+};
+
+static args_op op_arg[op_args_count] =
+{
+    {.op_name = "PUSH", .operation_code = OP_PUSH, .fn = ComparePush},
+    {.op_name = "POPR", .operation_code = OP_POPR, .fn = ComparePopR},
+    {.op_name = "PUSHR", .operation_code = OP_PUSHR, .fn = ComparePushR},
+    {.op_name = "JMP", .operation_code = OP_JMP, .fn = CompareJump},
+    {.op_name = "JB", .operation_code = OP_JB, .fn = CompareJB},
+}; 
+
+
+
+void ConvertToNative(asm_t* assembler);
+assembler_err NativeTranslator(asm_t* assembler, char* assembler_text);
+void FirstPass(asm_t* assembler);
--- assembler/src/convert_to_native_code.cpp
+++ assembler/src/convert_to_native_code.cpp
@@ -3,25 +3,42 @@
 #include <string.h>
 #include "convert_to_native_code.h"
 
-static args_fn op_arg[] = {ComparePush, ComparePopR, ComparePushR, CompareJump, CompareJB};
-static operation operation [] = 
+void FirstPass(asm_t* assembler)
 {
-    {.op_name = "POP", .operation_code = OP_POP},
-    {.op_name = "DUMP", .operation_code = OP_DUMP},
-    {.op_name = "MUL", .operation_code = OP_MUL},
-    {.op_name = "SUB", .operation_code = OP_SUB},
-    {.op_name = "OUT", .operation_code = OP_OUT},
-    {.op_name = "DIV", .operation_code = OP_DIV},
-    {.op_name = "HLT", .operation_code = OP_HLT}
-};
-const int op_args_count = 5;
-const int simple_op_count = 7;
+    assembler->instruction_pointer = 0;
+    for (size_t instruction_index = 0; instruction_index < assembler->size_file; instruction_index++)
+    {
+        for (size_t index = 0; index < op_args_count; index++)
+        {
+            printf("strlen1 %lu, strlen2 %lu\n", strlen(assembler->asm_code[instruction_index]), strlen(op_arg[index].op_name));
+            if((strlen(assembler->asm_code[instruction_index]) > strlen(op_arg[index].op_name)) &
+            (strncmp(assembler->asm_code[instruction_index], op_arg[index].op_name, strlen(op_arg[index].op_name)) == 0)) // TODO better strcmp 
+            {
+                printf("%s\n", assembler->asm_code[instruction_index]);
+                assembler->native_code = (int*)realloc(assembler->native_code, (assembler->size + 1) * sizeof(int));
+                ++assembler->size;
+                ++assembler->instruction_pointer;
+                break;
+            }
+        }
+
+        if(assembler->asm_code[instruction_index][0] == ':')
+        {
+            int label_index = atoi(assembler->asm_code[instruction_index] + 1);
+            printf("label %d\n", label_index);
+            labels[label_index] = assembler->instruction_pointer;
+            printf("ip %lu\n", assembler->instruction_pointer);
+            continue;
+        }
+        ++assembler->instruction_pointer;
+    }
+    assembler->instruction_pointer = 0;
+}
 
 void ConvertToNative(asm_t* assembler)
 {
-     
-    assembler->instruction_pointer = 0;
-    for (size_t asm_index = 0; assembler->instruction_pointer < assembler->size; asm_index++, assembler->instruction_pointer++)
+    FirstPass(assembler);
+    for (size_t asm_index = 0; assembler->instruction_pointer < assembler->size - 5; asm_index++, assembler->instruction_pointer++)
     {    
         if(NativeTranslator(assembler, assembler->asm_code[asm_index]) == ReadError)
         {
@@ -33,8 +50,7 @@ void ConvertToNative(asm_t* assembler)
 }
 
 assembler_err NativeTranslator(asm_t* assembler, char* assembler_text)
-{
-    
+{    
     for (size_t operation_index = 0; operation_index < simple_op_count; operation_index++)
     {
         if (strcmp(assembler_text, operation[operation_index].op_name) == 0)
@@ -45,7 +61,7 @@ assembler_err NativeTranslator(asm_t* assembler, char* assembler_text)
     }
     for (size_t function_index = 0; function_index < op_args_count; function_index++) // TODO remove magic number
     {
-        if (op_arg[function_index](assembler_text, assembler))
+        if (op_arg[function_index].fn(assembler_text, assembler))
         {
             return NoErr; 
         }   
@@ -54,15 +70,15 @@ assembler_err NativeTranslator(asm_t* assembler, char* assembler_text)
 }
 
 
+
+
 bool ComparePush(char* assembler_text, asm_t* assembler)
 {
     if(strncmp(assembler_text, "PUSH ", 5) == 0)
     {  
         char* number = assembler_text + 4;
-        assembler->native_code = (int*)realloc(assembler->native_code, (assembler->size + 1) * sizeof(int));
         assembler->native_code[assembler->instruction_pointer] = OP_PUSH; 
         assembler->native_code[++assembler->instruction_pointer] = atoi(number);
-        assembler->size += 1;
         return true;
     }
     return false;
@@ -73,10 +89,8 @@ bool ComparePushR(char* assembler_text, asm_t* assembler)
     if(strncmp(assembler_text, "PUSHR", 5) == 0)
     {  
         int reg = (int)assembler_text[6] - (int)'A'; // specific of processor
-        assembler->native_code = (int*)realloc(assembler->native_code, (assembler->size + 1) * sizeof(int));
         assembler->native_code[assembler->instruction_pointer] = OP_PUSHR; 
         assembler->native_code[++assembler->instruction_pointer] = reg;
-        assembler->size += 1;
         return true;
     }
     return false;
@@ -87,10 +101,8 @@ bool ComparePopR(char* assembler_text, asm_t* assembler)
     if(strncmp(assembler_text, "POPR", 4) == 0)
     {  
         int reg = (int)assembler_text[5] - (int)'A'; // specific of processor
-        assembler->native_code = (int*)realloc(assembler->native_code, (assembler->size + 1) * sizeof(int));
         assembler->native_code[assembler->instruction_pointer] = OP_POPR; 
         assembler->native_code[++assembler->instruction_pointer] = reg;
-        assembler->size += 1;
         return true;
     }
     return false;
@@ -101,17 +113,16 @@ bool CompareJump(char* assembler_text, asm_t* assembler)
     if(strncmp(assembler_text, "JMP", 3) == 0)
     {  
         char* number = assembler_text + 4;
-        assembler->native_code = (int*)realloc(assembler->native_code, (assembler->size + 1) * sizeof(int));
         assembler->native_code[assembler->instruction_pointer] = OP_JMP;
         if (number[0] == ':')
         {
-            
+            assembler->native_code[++assembler->instruction_pointer] = labels[atoi(number + 1)];
+
         }
         else 
         {
             assembler->native_code[++assembler->instruction_pointer] = atoi(number);
         }
-        assembler->size += 1;
         return true;
     }
     return false;
@@ -120,18 +131,16 @@ bool CompareJB(char* assembler_text, asm_t* assembler)
 {
     if(strncmp(assembler_text, "JB", 2) == 0)
     {  
-        char* number = assembler_text + 2;
-        assembler->native_code = (int*)realloc(assembler->native_code, (assembler->size + 1) * sizeof(int));
+        char* number = assembler_text + 3;
         assembler->native_code[assembler->instruction_pointer] = OP_JB;
         if (number[0] == ':')
         {
-            
+           assembler->native_code[++assembler->instruction_pointer] = labels[atoi(number + 1)];
         }
         else 
         {
             assembler->native_code[++assembler->instruction_pointer] = atoi(number);
         }
-        assembler->size += 1;
         return true;
     }
     return false;
